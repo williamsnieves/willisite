958e8f27be6c988cdb0233e8f77598780ba8d7b4
williamsnieves <williamsnieves@gmail.com>
2014-03-29 22:27:58 -0430

    haciendo responsive para tablets

---
+++ willisite/static/js/menu.js
@@ -0,0 +1,34 @@
+var colour = [
+	"rgb(142, 68, 173)",
+	"rgb(243, 156, 18)",
+	"rgb(211, 84, 0)",
+	"rgb(0, 106, 63)",
+	"rgb(41, 128, 185)",
+	"rgb(192, 57, 43)",
+	"rgb(135, 0, 0)",
+	"rgb(39, 174, 96)"
+];
+
+$(".free-wall .item").each(function() {
+	var backgroundColor = colour[colour.length * Math.random() << 0];
+	$(this).css({
+		backgroundColor: backgroundColor
+	});
+});
+
+$(function() {
+	var wall = new freewall("#freewall");
+	wall.reset({
+		selector: '.level1',
+		cellW: 320,
+		cellH: 160,
+		fixSize: 0,
+		gutterX: 20,
+		gutterY: 10,
+		onResize: function() {
+			wall.fitZone();
+		}
+	});
+	wall.fitZone();
+	$(window).trigger("resize");
+});
